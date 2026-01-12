297ff66ee428b5e9dad95fe1a021fe187d86ea5f
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2026-01-12 22:33:49 +0530

    Modified ProductController.js

--- Backend/product-service/src/controllers/productController.js
+++ Backend/product-service/src/controllers/productController.js
@@ -1,6 +1,21 @@
-import { getClothes } from "../services/productService.js";
+const productService = require('../services/productService');
 
-export const fetchClothes = (req, res) => {
-  const clothes = getClothes();
-  res.json(clothes);
+const getProducts = async (req, res) => {
+    try {
+        const products = await productService.getAllProducts();
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
+
+const addProduct = async (req, res) => {
+    try {
+        const newProduct = await productService.createProduct(req.body);
+        res.status(201).json(newProduct);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = { getProducts, addProduct };
